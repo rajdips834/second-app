7febebdf9e6efa387edf87f23c9138c023b57306
Rajdip Sinha <maverick@Rajdips-MacBook-Pro.local>
2023-03-11 13:03:25 +0530

    stable

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,31 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Newlyweds } from 'src/interfaces/newlywed';
+import { TimingDetails } from 'src/interfaces/timingDetails';
 import { VenueDetails } from 'src/interfaces/venueDetais';
+import { DataService } from 'src/services/data.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
-  timeDetails={
-    reception:'Saturday | 13th May 2023 | 7:30PM',
-    wedding:'Sunday | 14th May 2023 | 7:45 AM'
-  }
-  venueDetails: VenueDetails = {
-    
-    addressLine1: "Babayaga",
-    addressLine2:'Deeznuts'
+export class DetailsComponent implements OnInit {
+  timeDetails?:TimingDetails;
+  venueDetails?: VenueDetails;
 
+  newlyweds?:Newlyweds;
 
-  }
-  newlyweds:Newlyweds={
-    bride: 'Meghana',
-    groom:'Anirban'
+  constructor(private data: DataService){}
+
+  ngOnInit() {
+    this.newlyweds=this.data.getnewlyWeds()
+    this.venueDetails=this.data.getvenue()
+    this.timeDetails=this.data.gettimedetails()
   }
 
   foo(e: any) {
-
     console.log(e);
   }
 }
